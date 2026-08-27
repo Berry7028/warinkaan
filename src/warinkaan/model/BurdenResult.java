10984f83088fd4b8815662b65a34f26cf6a39968
Berry7028 <s.kouta7028@gmail.com>
2026-08-27 17:36:38 +0900

    fix: burden resultクラスにコンストラクターの追加

--- src/warinkaan/model/BurdenResult.java
+++ src/warinkaan/model/BurdenResult.java
@@ -5,6 +5,13 @@ public class BurdenResult {
 	String participantName;
 	int burdenAmount;
 
+	public BurdenResult(int participantId, String participantName, int burdenAmount) {
+		super();
+		this.participantId = participantId;
+		this.participantName = participantName;
+		this.burdenAmount = burdenAmount;
+	}
+
 	public int getParticipantId() {
 		return participantId;
 	}
