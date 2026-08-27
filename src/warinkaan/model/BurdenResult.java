4b52eaf9ac7f2e0249d3a8cf1902abbfc1c5dbde
Berry7028 <s.kouta7028@gmail.com>
2026-08-27 17:35:33 +0900

    feat: burden resultを実装

--- src/warinkaan/model/BurdenResult.java
+++ src/warinkaan/model/BurdenResult.java
@@ -1,5 +1,20 @@
 package warinkaan.model;
 
 public class BurdenResult {
+	int participantId;
+	String participantName;
+	int burdenAmount;
+
+	public int getParticipantId() {
+		return participantId;
+	}
+
+	public String getParticipantName() {
+		return participantName;
+	}
+
+	public int getBurdenAmount() {
+		return burdenAmount;
+	}
 
 }
