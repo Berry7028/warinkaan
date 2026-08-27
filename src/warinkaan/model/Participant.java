116916650f1df4c6329a76fce109a430c35fff9a
Berry7028 <s.kouta7028@gmail.com>
2026-08-27 17:20:37 +0900

    feat: paricipant modelを実装

--- src/warinkaan/model/Participant.java
+++ src/warinkaan/model/Participant.java
@@ -1,5 +1,32 @@
 package warinkaan.model;
 
 public class Participant {
+	int paparticipantId;
+	String name;
+	int weight;
+
+	public int getPaparticipantId() {
+		return paparticipantId;
+	}
+
+	public void setPaparticipantId(int paparticipantId) {
+		this.paparticipantId = paparticipantId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
 
 }
